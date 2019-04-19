392d534443b88fa05633a34dadec9a01b736b94a
Abhishek K <abhishekakade@gmail.com>
2019-04-19 21:48:48 +0530

    Updates

--- precache-manifest.bf4bd6fa793777d714b92712226ee7b3.js
+++ precache-manifest.bf4bd6fa793777d714b92712226ee7b3.js
@@ -1,26 +1,30 @@
 self.__precacheManifest = [
   {
+    "revision": "ab3e32da22ee21d8a73519f57cb4b325",
+    "url": "/random-quote-machine/static/media/icomoon.ab3e32da.ttf"
+  },
+  {
     "revision": "1a6e4adae3f8c7c8193b",
     "url": "/random-quote-machine/static/js/runtime~main.1a6e4ada.js"
   },
   {
-    "revision": "e3e9e52af62f26723c56",
-    "url": "/random-quote-machine/static/js/main.e3e9e52a.chunk.js"
+    "revision": "9c0b1454a42e4c454178",
+    "url": "/random-quote-machine/static/js/main.9c0b1454.chunk.js"
   },
   {
     "revision": "713abdb80c60f1dfaccb",
     "url": "/random-quote-machine/static/js/2.713abdb8.chunk.js"
   },
   {
-    "revision": "e3e9e52af62f26723c56",
-    "url": "/random-quote-machine/static/css/main.5a387454.chunk.css"
+    "revision": "9c0b1454a42e4c454178",
+    "url": "/random-quote-machine/static/css/main.1b63f5c7.chunk.css"
   },
   {
     "revision": "713abdb80c60f1dfaccb",
     "url": "/random-quote-machine/static/css/2.f73da50d.chunk.css"
   },
   {
-    "revision": "93dce61b0e450ca0bc05c3d1f6133a5c",
+    "revision": "3166462a53888515fc1c515de73ea999",
     "url": "/random-quote-machine/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/random-quote-machine/precache-manifest.533a94c00ec1b9b8f6d4af2325d8591d.js"
+  "/random-quote-machine/precache-manifest.bf4bd6fa793777d714b92712226ee7b3.js"
 );
 
 workbox.clientsClaim();
