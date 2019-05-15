51ad565613b8408791633e769142fae566f98317
Abhishek K <abhishekakade@gmail.com>
2019-05-15 11:25:43 +0530

    Updates

---
+++ precache-manifest.8ebfec5d63af359bf413945a61515ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f81a24f5fc9c73372dea48d7288be1c",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "8b6175ea83b01f188600",
+    "url": "/random-quote-machine/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "349bc9325b7cac0c11f4",
+    "url": "/random-quote-machine/static/css/main.57c924eb.chunk.css"
+  },
+  {
+    "revision": "8b6175ea83b01f188600",
+    "url": "/random-quote-machine/static/js/2.0cc6d4fc.chunk.js"
+  },
+  {
+    "revision": "349bc9325b7cac0c11f4",
+    "url": "/random-quote-machine/static/js/main.81b154d2.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  },
+  {
+    "revision": "ab3e32da22ee21d8a73519f57cb4b325",
+    "url": "/random-quote-machine/static/media/icomoon.ab3e32da.ttf"
+  }
+]);
